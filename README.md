fbb11a6ff88b0e066b041bcd779d58b44d03d69b
DaveOke <david.oke@hexagongeospatial.com>
2017-07-03 22:24:17 -0400

    Add files via upload

---
+++ README.md
@@ -0,0 +1,19 @@
+﻿#PlexIsyIntegrator
+
+## Synopsis
+
+This little tool provides a Plex Webhook server which along with a ISY controller, allows automatially controls your theatre lighting.
+
+## Configuration
+
+Edit app.js to enter your ISY credentials. 
+Start the service
+Set up a plex webhook to point at the service
+Start a movie to get your plex client uuid
+Register your plex client in app.js, set which lights you want controlled and at what levels
+
+	registerPlexClient("plex_client_uuid", "ISY Device Name", 25, 255, 25, 80);
+
+## License
+
+Licensed under MIT
---
+++ app.js
@@ -0,0 +1,101 @@
+'use strict';
+
+var express = require("express"),
+    multer = require('multer'),
+    request = require("request").defaults({ strictSSL: false }),
+    parseString = require('xml2js').parseString,
+    express = require("express"),
+    multer = require('multer');
+
+// START CONFIGURATION HERE.
+
+// Plex Webhook Port 
+var port = 90;
+
+// ISY Config
+var username = "admin"
+var password = "admin"
+var isy_ip = "https://10.0.1.5"
+
+// Actions (Plex Client uuid, play light level, stop level, resume level, pause level)
+// Levels range from 0-255
+// To get your uuid, run this app and start a movie using your player
+// the uuid of your player will be shown in the console log.
+function registerClients() {
+    registerPlexClient("wti8krp6qezy3nmi", "Family Room Lights", 25, 255, 25, 80);
+}
+
+// END CONFIGURATION - Shouldn't need to touch anything below this line.
+
+function registerPlexClient(clientId, isyNodeName, playLightLevel, stopLightLevel, resumeLightLevel, pauseLightLevel) {
+
+    var isyNodeKey = nodeNameToAddressMap[isyNodeName];
+
+    var newClient = {};
+    newClient[clientId] = {
+        "media.pause": () => {
+            request(isycmd(isyNodeKey + "/cmd/DON/" + pauseLightLevel));
+        },
+        "media.stop": () => {
+            request(isycmd(isyNodeKey + "/cmd/DON/" + stopLightLevel));
+        },
+        "media.play": () => {
+            request(isycmd(isyNodeKey + "/cmd/DON/" + playLightLevel));
+        },
+        "media.resume": () => {
+            request(isycmd(isyNodeKey + "/cmd/DON/" + resumeLightLevel));
+        }
+    }
+
+    Object.assign(registeredClients, newClient);
+}
+
+var registeredClients = {};
+var nodeNameToAddressMap = {};
+var auth = "Basic " + new Buffer(username + ":" + password).toString("base64");
+
+function isycmd(cmd) {
+    return {
+        method: 'GET',
+        url: isy_ip + '/rest/nodes/' + cmd,
+        headers: {
+            "Authorization": auth
+        }
+    }
+};
+
+function getISYNodes() {
+    request(isycmd(''), (error, response, body) => {
+        var nodes = parseString(body, (err, result) => {
+            result.nodes.node.forEach((value) => {
+                console.log("Registered Node Name: " + value.name[0] + " as Node Id: " + value.address[0]);
+                nodeNameToAddressMap[value.name[0]] = value.address[0];
+            });
+        });
+        registerClients();
+        app.listen(port, () => {
+            console.log("Started on port:" + port);
+        });
+    });
+}
+
+var upload = multer({ dest: '/tmp/' });
+var app = express();
+
+app.post("/", upload.single('thumb'), (req, res, next) => {
+    var payload = JSON.parse(req.body.payload);
+
+    var handled = false;
+    if (registeredClients[payload.Player.uuid]) {
+        if (registeredClients[payload.Player.uuid][payload.event])
+            registeredClients[payload.Player.uuid][payload.event]();
+    }
+    else {
+        console.log("Event from unhandled device: " + payload.Player.uuid + ", event: " + payload.event);
+    }
+    
+    res.sendStatus("200");
+});
+
+// Main entry point
+getISYNodes();
